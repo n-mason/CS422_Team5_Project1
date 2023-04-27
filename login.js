92e458d91594e581d773cdf9fbc8afa766ebe038
HanSa0810 <hcsagalyn@gmail.com>
2023-04-26 17:51:03 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+function MLEloginclick() {
+    var x = document.getElementById("MLEbox");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function CONTloginclick() {
+    var x = document.getElementById("CONTbox");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
