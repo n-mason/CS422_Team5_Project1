9edd255ac68d1fc059c7fd3802935e431bab8aba
Cgv <cgv@uoregon.edu>
2023-04-29 12:32:08 -0700

    updated all functions to work with 2D arrays. made graph function that prints. Began function that calculates all errors and passes them as graphs

--- error_algorithms.py
+++ error_algorithms.py
@@ -1,11 +1,11 @@
 import sys
 import math
 import csv
-from flask_Proj1 import file_to_df
+#from flask_Proj1 import file_to_df
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plp
 
-#TODO: update all functions to work with 2D arrays
 #TODO: update both array functions to look for date column and cull it
 
 def csv_to_arr(file):
@@ -25,7 +25,7 @@ def csv_to_arr(file):
 
 def dataframe_to_array(dataframe):
     array = dataframe.to_numpy()
-    array = np.delete(array,0,1)
+    #array = np.delete(array,0,1)
     return array
 
 def mean_absolute_error(forecastResult, testSet): #formula taken from https://en.wikipedia.org/wiki/Mean_absolute_error
@@ -44,70 +44,95 @@ def mean_absolute_error(forecastResult, testSet): #formula taken from https://en
     return error
 
 def mean_absolute_percentage_error(forecastResult, testSet): #formula taken from https://en.wikipedia.org/wiki/Mean_absolute_percentage_error
-    error = 0
-    length = len(forecastResult)
-    if length != len(testSet):
+    error = [0]*len(forecastResult[0])
+    length = len(forecastResult[0])
+    if length != len(testSet[0]):
         sys.exit("Error: incompatible array sizes") #if the array sizes don't match, causes an error
-    i = 0
-    while i < length: #while loop with the sigma formula
-        error += abs((testSet[i]-forecastResult[i])/testSet[i])
-        i += 1
-    return round(((100*error)/length),2)
+    for i in range(len(forecastResult)):
+        j = 0
+        while j < length: #while loop with the sigma formula
+            error[j] += abs((testSet[i][j]-forecastResult[i][j])/testSet[i][j])
+            j += 1
+    for j in range(len(error)):
+        error[j] *= 100/len(forecastResult)
+    return error
 
 def symmetric_mean_absolute_percentage_error(forecastResult, testSet): #formula taken from https://en.wikipedia.org/wiki/Symmetric_mean_absolute_percentage_error
-    error = 0
-    length = len(forecastResult)
-    if length != len(testSet):
+    error = [0] * len(forecastResult[0])
+    length = len(forecastResult[0])
+    if length != len(testSet[0]):
         sys.exit("Error: incompatible array sizes") #if the array sizes don't match, causes an error
-    i = 0
-    while i < length: #while loop with the sigma formula
-        change = abs(forecastResult[i]-testSet[i])
-        change /= (abs(testSet[i]) + abs(forecastResult[i]))/2
-        error += change
-        i += 1
-    return round(((100*error)/length),2)
+    for i in range(len(forecastResult)):
+        j = 0
+        while j < length: #while loop with the sigma formula
+            change = abs(forecastResult[i][j]-testSet[i][j])
+            change /= (abs(testSet[i][j]) + abs(forecastResult[i][j]))/2
+            error[j] += change
+            j += 1
+    for j in range(len(error)):
+        error[j] *= 100/len(forecastResult)
+    return error
 
 def mean_squared_error(forecastResult, testSet): #formula taken from https://en.wikipedia.org/wiki/Mean_squared_error
-    error = 0
-    length = len(forecastResult)
-    if length != len(testSet):
+    error = [0] * len(forecastResult[0])
+    length = len(forecastResult[0])
+    if length != len(testSet[0]):
         sys.exit("Error: incompatible array sizes") #if the array sizes don't match, causes an error
-    i = 0
-    while i < length: #while loop with the sigma formula
-        error += ((testSet[i]-forecastResult[i])**2)
-        i += 1
-    return round((error/length),2)
+    for i in range(len(forecastResult)):
+        j = 0
+        while j < length: #while loop with the sigma formula
+            error[j] += ((testSet[i][j]-forecastResult[i][j])**2)
+            j += 1
+    for j in range(len(error)):
+        error[j] /= len(forecastResult)
+    return error
 
 def root_mean_squared_error(forecastResult, testSet): #formula taken from https://en.wikipedia.org/wiki/Root-mean-square_deviation
-    error = 0
-    length = len(forecastResult)
-    if length != len(testSet):
-        sys.exit("Error: incompatible array sizes") #if the array sizes don't match, causes an error
-    i = 0
-    while i < length: #while loop with the sigma formula
-        error += ((testSet[i]-forecastResult[i])**2)
-        i += 1
-    return round(math.sqrt(error / length), 2)
+    error = mean_squared_error(forecastResult,testSet)
+    for i in range(len(error)):
+        error[i] = math.sqrt(error[i])
+    return error
 
-def correlation_coefficient(forecastResults, testSet): #formula taken from https://www.youtube.com/watch?v=11c9cs6WpJU
-    length = len(forecastResults)
-    if length != len(testSet):
+def correlation_coefficient(forecastResult, testSet): #formula taken from https://www.youtube.com/watch?v=11c9cs6WpJU
+    length = len(forecastResult[0])
+    size = len(forecastResult)
+    if length != len(testSet[0]):
         sys.exit("Error: incompatible array sizes")  # if the array sizes don't match, causes an error
-    i, sumForecast, sumTest, sumBoth, forecastSquared, testSquared = 0,0,0,0,0,0
-    while i < length:
-        sumForecast += forecastResults[i]
-        sumTest += testSet[i]
-        sumBoth += forecastResults[i] * testSet[i]
-        forecastSquared += forecastResults[i]**2
-        testSquared += testSet[i]**2
-        i += 1
-    numerator = (length*sumBoth)-(sumForecast*sumTest)
-    denominator = math.sqrt(((length*forecastSquared) - (sumForecast**2))*((length*testSquared) - (sumTest**2)))
-    return round((numerator/denominator), 4)
+    sumForecast, sumTest, sumBoth, forecastSquared, testSquared = [0] * len(forecastResult[0]), [0] * len(forecastResult[0]), [0] * len(forecastResult[0]), [0] * len(forecastResult[0]), [0] * len(forecastResult[0])
+    for i in range(size):
+        j = 0
+        while j < length:
+            sumForecast[j] += forecastResult[i][j]
+            sumTest[j] += testSet[i][j]
+            sumBoth[j] += forecastResult[i][j] * testSet[i][j]
+            forecastSquared[j] += forecastResult[i][j]**2
+            testSquared[j] += testSet[i][j]**2
+            j += 1
+    error = [0] * len(forecastResult[0])
+    for j in range(len(error)):
+        numerator = (size*sumBoth[j])-(sumForecast[j]*sumTest[j])
+        denominator = math.sqrt(((size*forecastSquared[j]) - (sumForecast[j]**2))*((size*testSquared[j]) - (sumTest[j]**2)))
+        error[j] = numerator/denominator
+    return error
 
-def error_arr_to_plot(error_array):
-    #TODO: implement
-    return
+def error_arr_to_plot(error_array,title):
+    x_axis = [0]*len(error_array)
+    for i in range(len(error_array)):
+        x_axis[i] = i
+    plp.title(title)
+    plp.xlabel("columns")
+    plp.ylabel("error values")
+    plp.plot(x_axis,error_array,color='red')
+    plp.show()
+    return 0
+
+def the_big_one(forecastResult,testSet):
+    meanError = mean_absolute_percentage_error(forecastResult,testSet)
+    symmetricMeanError = symmetric_mean_absolute_percentage_error(forecastResult,testSet)
+    rootError = root_mean_squared_error(forecastResult,testSet)
+    correlationCoefficient = correlation_coefficient(forecastResult,testSet)
+
+
 
 
 
@@ -120,13 +145,16 @@ def main():
     with open('GOOG_test_set.csv','r') as file2:
         test = csv_to_arr(file2)
         #print(test)
-    mean_absolute_error(forecast,test)
+    error_arr_to_plot(correlation_coefficient(forecast,test),"correlation coefficient")
 
-    dataframe = file_to_df('GOOG_MLE_upload.csv','.csv')
-    dataframe2 = file_to_df('GOOG_test_set.csv', '.csv')
-    forecast = dataframe_to_array(dataframe)
-    observed = dataframe_to_array(dataframe2)
-    mean_absolute_error(forecast,observed)
+
+    '''
+    dataframe = file_to_df(GOOG_MLE_upload.csv,'.csv')
+    dataframe2 = file_to_df(GOOG_test_set.csv, '.csv')
+    #forecast = dataframe_to_array(dataframe)
+    #observed = dataframe_to_array(dataframe2)
+    #mean_absolute_percentage_error(forecast,observed)
+    '''
 
 main()
 
